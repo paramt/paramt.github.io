456edb6e87785834d3f42da393e9772251b8f629
Param Thakkar <pht0104@gmail.com>
2019-04-18 20:01:46 -0400

    Redirect 404 errors to similar URLs Closes #3

---
+++ resources/js/404.js
@@ -0,0 +1,19 @@
+var query = document.location.href.split("param.me/")[1]
+var directories = ["1pt", "code-editor", "discord-emoji", "email-status", "kevlar", "mathu", "mathu-prototype", "meme", "MemeAdviser", "old", "projects", "sanskrit-quotes-calendar", "trackarma"]
+var distances = []
+var closest;
+
+for(var i = 0; i<directories.length; i++){
+  var distance = getEditDistance(query.toLowerCase(), directories[i])
+  
+  if(distance < Math.min(...distances) && distance < Math.floor(directories[i].length/2)){
+    closest = directories[i];
+    distances.push(distance);
+  }
+}
+
+if(closest){
+  window.location.href = "https://www.param.me/" + closest;
+} else {
+  window.location.href;
+}
