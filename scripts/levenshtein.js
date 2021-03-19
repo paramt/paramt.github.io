1e6116c80067a75ec48f004a697958e5fe7172d5
Param Thakkar <pht0104@gmail.com>
2021-03-19 17:41:54 -0400

    Refactor
    - Reset all styles and scripts
    - Remove animate.css (use cdn)

--- scripts/levenshtein.js
+++ scripts/levenshtein.js
@@ -6,36 +6,40 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */
 
 // Compute the edit distance between the two given strings
-getEditDistance = function(a, b){
+getEditDistance = function (a, b) {
   a = a.toLowerCase();
   b = b.toLowerCase();
-  
-  if(a.length == 0) return b.length; 
-  if(b.length == 0) return a.length; 
+
+  if (a.length == 0) return b.length;
+  if (b.length == 0) return a.length;
 
   var matrix = [];
 
   // increment along the first column of each row
   var i;
-  for(i = 0; i <= b.length; i++){
+  for (i = 0; i <= b.length; i++) {
     matrix[i] = [i];
   }
 
   // increment each column in the first row
   var j;
-  for(j = 0; j <= a.length; j++){
+  for (j = 0; j <= a.length; j++) {
     matrix[0][j] = j;
   }
 
   // Fill in the rest of the matrix
-  for(i = 1; i <= b.length; i++){
-    for(j = 1; j <= a.length; j++){
-      if(b.charAt(i-1) == a.charAt(j-1)){
-        matrix[i][j] = matrix[i-1][j-1];
+  for (i = 1; i <= b.length; i++) {
+    for (j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) == a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
       } else {
-        matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
-                                Math.min(matrix[i][j-1] + 1, // insertion
-                                         matrix[i-1][j] + 1)); // deletion
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j - 1] + 1, // substitution
+          Math.min(
+            matrix[i][j - 1] + 1, // insertion
+            matrix[i - 1][j] + 1
+          )
+        ); // deletion
       }
     }
   }
