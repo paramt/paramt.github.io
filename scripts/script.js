981b23df303e7961ec9e7e10df8253119a7f078e
Param Thakkar <pht0104@gmail.com>
2021-03-24 17:19:02 -0400

    Fix tabs overlapping cards

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,24 @@ history.scrollRestoration = "manual";
 function moveUp(element, id) {
   document.querySelector(id).style.transform = "translateY(-15px)";
   element.style.transform = "translateY(-15px)";
-  element.style.cursor = "pointer";
 }
 
 function moveDown(element, id) {
   document.querySelector(id).style.transform = "translateY(0px)";
   element.style.transform = "translateY(0px)";
-  element.style.cursor = "default";
 }
 
-function switchTab(id) {
-  document.querySelector("#about").classList.remove("active");
-  document.querySelector("#projects").classList.remove("active");
-  document.querySelector("#item3").classList.remove("active");
-  document.querySelector("#achievements").classList.remove("active");
+function switchTab(element, id) {
+  document.querySelectorAll(".card").forEach((tab) => {
+    tab.classList.remove("active");
+  });
+
+  document.querySelectorAll(".tab").forEach((tab) => {
+    tab.classList.remove("active");
+  });
 
   document.querySelector(id).classList.add("active");
+  element.classList.add("active");
 }
 
 window.setTimeout(() => {
