a130d764d656cfcebdd5e4b5cfacf757eea5a3ca
Param Thakkar <pht0104@gmail.com>
2022-03-05 01:56:42 -0500

    Implement "last updated" feature

--- scripts/script.js
+++ scripts/script.js
@@ -39,3 +39,53 @@ function switchTab(element, id) {
 window.setTimeout(() => {
   document.body.style.overflow = "visible";
 }, 2000);
+
+window.onload = function() {
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(xhttp.responseText);
+        var date = new Date(Date.parse(data[0].commit.author.date));
+        document.getElementById("last-updated").innerHTML = timeSince(date) + " ago";
+      }
+  };
+  xhttp.open("GET", "https://api.github.com/repos/paramt/paramt.github.io/commits?path=_config.yml&page=1&per_page=1", true);
+  xhttp.send();
+
+}
+
+function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years";
+  }
+
+  interval = seconds / 2592000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " months";
+  }
+
+  interval = seconds / 86400;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " days";
+  }
+
+  interval = seconds / 3600;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " hours";
+  }
+
+  interval = seconds / 60;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes";
+  }
+
+  return Math.floor(seconds) + " seconds";
+}
