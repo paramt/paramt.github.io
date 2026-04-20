5d9d4949595fb11a93ffb6285829e9c1b194e23d
Param Thakkar <pht0104@gmail.com>
2026-04-20 18:32:12 -0400

    add hero polaroid

--- src/data/heroPolaroids.js
+++ src/data/heroPolaroids.js
@@ -2,6 +2,7 @@ import video3 from './images/hero/3/3.mp4';
 import video7 from './images/hero/7/IMG_3183.mp4';
 import video8 from './images/hero/8/IMG_1415.mp4';
 
+import thumbLinkedin4 from './images/hero/linkedin-pfp-4.thumb.webp';
 import thumb1 from './images/hero/1.thumb.webp';
 import thumb2 from './images/hero/2.thumb.webp';
 import thumb3 from './images/hero/3/3.thumb.webp';
@@ -13,6 +14,7 @@ const heroPolaroids = [
   { image: thumb3, video: video3, w: 4284, h: 5712, location: 'Key West, FL', date: '12/16/25' },
   { image: thumb8, video: video8, w: 4032, h: 3024, location: 'Big Sur, CA', date: '05/26/25' },
   { image: thumb1, w: 2539, h: 3386, location: 'Manhattan, NY', date: '03/19/26' },
+  { image: thumbLinkedin4, w: 3024, h: 4032, location: 'Vancouver, BC', date: '08/16/24' },
   { image: thumb2, w: 1600, h: 1200, location: 'Spring Hill, FL', date: '02/17/26' },
   { image: thumb5, w: 1536, h: 2048, location: 'Mountain View, CA', date: '05/13/25' },
   { image: thumb7, video: video7, w: 4284, h: 5712, location: 'Ottawa, ON', date: '10/16/25' },
